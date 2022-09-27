61ed60de5d3d7c88ffdccb418ff048fa6de031a5
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-27 12:10:02 -0300

    att

---
+++ bitcoin/src/components/QuotetionList/QuotationItem/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {View, Text, Image} from 'react-native'
+import styles from './style'
+
+export default function CurrentPrice(){
+    return(
+        <View style={styles.mainContent} >
+
+           <View>
+               <View>
+                <Image/>
+                <Text/>
+               </View>
+               <View>
+                   <Text></Text>
+               </View>
+           </View>
+        </View>
+    )
+}
---
+++ bitcoin/src/components/QuotetionList/QuotationItem/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet  } from 'react-native';
+
+const styles = StyleSheet.create({
+    mainContent:{
+        widht:"95%",
+        height:"auto",
+        backgroundColor:"#fff",
+        marginLeft:"3%",
+        marginBottom:15,
+    },
+ 
+});
+
+ export default styles
